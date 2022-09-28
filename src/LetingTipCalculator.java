a644a112739b82a30b766bad91c788176c6dd4eb
LetingCai <letingc@nycstudents.net>
2022-09-28 12:31:42 -0400

    First day on project

---
+++ src/LetingTipCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class LetingTipCalculator {
+    public static void main(String[] args){
+        //Scanner Set Up//
+        Scanner scan = new Scanner(System.in);
+
+        //Asking for user input to set up variables//
+        System.out.println("How much is your bill?");
+        double billAmount = scan.nextDouble;
+
+
+    }
+}
