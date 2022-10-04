77ab28befae741f607553cbef772bbedd98bc11a
Leting Cai <letingc@nycstudents.net>
2022-10-04 14:43:06 -0400

    First day on project

--- src/LetingTipCalculator.java
+++ src/LetingTipCalculator.java
@@ -1,3 +1,4 @@
+//import scanner
 import java.util.Scanner;
 
 public class LetingTipCalculator {
@@ -6,15 +7,26 @@ public class LetingTipCalculator {
         Scanner scan = new Scanner(System.in);
 
         //Asking for user input to set up variables//
-        System.out.println("How much is your bill?");
-        double billAmount = scan.nextDouble;
+        System.out.print("Enter bill amount: ");
+        double billAmount = scan.nextDouble();
         
-        Sytem.out.println("Number of people");
-        int numberOfPeople = scan.nextInt;
+        System.out.print("Enter the number of people: ");
+        int numberOfPeople = scan.nextInt();
         
-        System.out.println("Tip Percentage?");
-        
+        System.out.print("Enter tip Percentage: ");
+        int tipPercentage = scan.nextInt();
 
+        //Initializing and creating variables.
+        double totalBill = Math.round(100*billAmount*(tipPercentage/100.0+1))/100.0;
+        double totalTip = Math.round(100*billAmount*(tipPercentage/100.0))/100.0;
+        double tipPerPerson = Math.round(totalTip/numberOfPeople*100)/100.0;
+        double billPerPerson = Math.round(totalBill/numberOfPeople*100)/100.0;
 
+        //Display result
+        System.out.println();
+        System.out.println("Total bill amount(including tip): "+totalBill);
+        System.out.println("Total tip amount: "+ totalTip);
+        System.out.println("Total bill amount per person: "+billPerPerson);
+        System.out.println("Total tip amount per person: "+tipPerPerson);
     }
 }
